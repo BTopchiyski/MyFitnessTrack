fd6b1b9c62ed769ab0983355190b3826feb57fa7
BTopchiyski <topchiisky@gmail.com>
2023-05-26 13:13:47 +0300

    PreAuthorize and CLI Admin/Manager added

--- myfitnesstrack-api/src/main/java/com/myfitnesstrack/myfitnesstrackapi/MyfitnesstrackApiApplication.java
+++ myfitnesstrack-api/src/main/java/com/myfitnesstrack/myfitnesstrackapi/MyfitnesstrackApiApplication.java
@@ -17,4 +17,29 @@ public class MyfitnesstrackApiApplication {
 		SpringApplication.run(MyfitnesstrackApiApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			AuthenticationService service
+	) {
+		return args -> {
+			var admin = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("admin@mail.com")
+					.password("password")
+					.role(ADMIN)
+					.build();
+			System.out.println("Admin token: " + service.register(admin).getAccessToken());
+
+			var manager = RegisterRequest.builder()
+					.firstname("Manager")
+					.lastname("Manager")
+					.email("manager@mail.com")
+					.password("password")
+					.role(MANAGER)
+					.build();
+			System.out.println("Manager token: " + service.register(manager).getAccessToken());
+
+		};
+	}
 }
